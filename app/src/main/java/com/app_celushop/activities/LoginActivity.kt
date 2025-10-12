0a820a17c5ff24c98fcb0ad07b261e2988e96bea
Diana Isabel <firewolfnight9175@hotmail.com>
2025-10-12 16:50:49 -0500

    Initial commit

---
+++ app/src/main/java/com/app_celushop/activities/LoginActivity.kt
@@ -0,0 +1,27 @@
+package com.app_celushop.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.app_celushop.MainActivity
+import com.app_celushop.R
+
+class LoginActivity : AppCompatActivity() {
+    private val SPLASH_DURATION = 3000L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, PerfilActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_DURATION)
+    }
+}
